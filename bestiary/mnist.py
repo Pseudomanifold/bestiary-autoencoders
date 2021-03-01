c46eaf6d7b5d0c6cd8dc81ead973c523e51ff30a
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-01 19:16:24 +0100

    Added simple visualisation of trained model

--- bestiary/mnist.py
+++ bestiary/mnist.py
@@ -1,9 +1,17 @@
 """Demonstrate the use of different autoencoder models for MNIST."""
 
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import torch
+
 from .data import MNISTDataModule
 from .models import LinearAutoencoder
 
 from pytorch_lightning import Trainer
+
+from torchvision.utils import make_grid
 
 
 if __name__ == '__main__':
@@ -12,5 +20,20 @@ if __name__ == '__main__':
     dm.setup()
 
     model = LinearAutoencoder(input_dim=dm.input_dim)
-    trainer = Trainer()
+
+    trainer = Trainer(
+        max_epochs=10
+    )
     trainer.fit(model, dm)
+
+    with torch.no_grad():
+        x, y = next(iter(dm.test_dataloader()))
+        z, loss = model(x.view(x.size(0), -1))
+
+        batch_size, dim = z.size()
+        width, height = int(np.sqrt(dim)), int(np.sqrt(dim))
+        z = z.view(batch_size, 1, height, width)
+
+    img = make_grid(z)
+    plt.imshow(np.transpose(img, (1, 2, 0)))
+    plt.show()
