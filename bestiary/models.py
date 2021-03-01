fc1fc4c9b9e7c176a17bf12d07c22767362b7a25
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-01 17:43:14 +0100

    Started implementing the first model

---
+++ bestiary/models.py
@@ -0,0 +1,44 @@
+"""Implementation of some autoencoder architectures."""
+
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import pytorch_lightning as pl
+
+
+class LinearAutoencoder(pl.LightningModule):
+    """A simple linear autoencoder architecture."""
+
+    def __init__(self, input_dim, bottleneck_dim=2):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, bottleneck_dim),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(bottleneck_dim, input_dim)
+        )
+
+        self.loss_fn = F.mse_loss
+
+    def forward(self, x):
+        embedding = self.encoder(x)
+        return embedding
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        #x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = self.loss_fn(x_hat, x)
+
+        self.log('train_loss', loss)
+
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
