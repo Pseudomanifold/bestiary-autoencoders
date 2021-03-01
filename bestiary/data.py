f02c67375b41b5d754013306a3965c5dfc23923f
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-01 18:28:16 +0100

    Storing input dimensions of MNIST data set for simplicity

--- bestiary/data.py
+++ bestiary/data.py
@@ -1,7 +1,7 @@
 """Data sets for use with `pytorch_lightning`."""
 
 from torch.utils.data import DataLoader
-from torch.utils_data import random_split
+from torch.utils.data import random_split
 
 from pytorch_lightning import LightningDataModule
 
@@ -19,19 +19,25 @@ class MNISTDataModule(LightningDataModule):
         self.root = os.path.join(os.getcwd(), 'data')
 
     def prepare_data(self):
-        MNIST(self.root, train=True, download=True, transforms=ToTensor())
-        MNIST(self.root, train=False, download=True, transforms=ToTensor())
+        MNIST(self.root, train=True, download=True, transform=ToTensor())
+        MNIST(self.root, train=False, download=True, transform=ToTensor())
 
-    def setup(self, stage):
+    def setup(self):
         train_data = MNIST(self.root, train=True, transform=ToTensor())
         test_data = MNIST(self.root, train=False, transform=ToTensor())
 
-        # MNIST is suffiicently large so that we can pull this off!
+        # MNIST is sufficiently large so that we can pull this off!
         train_data, val_data = random_split(train_data, [55000, 5000])
 
         self.train_dataset = train_data
         self.val_dataset = val_data
         self.test_dataset = test_data
+
+        # We could also hard-code this, but let's get dimensions and
+        # other information from the data.
+
+        batch, labels = next(iter(self.train_dataloader()))
+        self.input_dim = batch.view(self.batch_size, -1).size()[1]
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size)
