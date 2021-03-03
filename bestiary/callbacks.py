e8f9cb60eb4a2206fe5cea5a973e1002ce889400
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-03 17:26:15 +0100

    Added callback function for storing images

---
+++ bestiary/callbacks.py
@@ -0,0 +1,41 @@
+"""Callback modules for executing tasks during training."""
+
+from pytorch_lightning.callbacks.base import Callback
+
+from torchvision.utils import save_image
+
+import numpy as np
+
+import torch
+import os
+
+
+class SaveLatentSpaceCallback(Callback):
+
+    def __init__(self, output_dir):
+        super().__init__()
+        self.output_dir = output_dir
+
+        os.makedirs(output_dir, exist_ok=True)
+
+    def on_epoch_end(self, trainer, pl_module):
+        with torch.no_grad():
+            x, y = next(iter(pl_module.val_dataloader()))
+            x_hat, z, loss = pl_module(x.view(x.size(0), -1))
+
+            batch_size, dim = x_hat.size()
+            width, height = int(np.sqrt(dim)), int(np.sqrt(dim))
+            x_hat = x_hat.view(batch_size, 1, height, width)
+
+        epoch = trainer.current_epoch
+
+        save_image(
+            x_hat,
+            os.path.join(self.output_dir, f'R_{epoch:04d}.png')
+        )
+
+        np.savetxt(
+            os.path.join(self.output_dir, f'L_{epoch:04d}.txt'),
+            z.numpy(),
+            fmt='%.8f'
+        )
