3be84c812f78ae2849f61f046069480f8e1ad15e
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-08 10:08:45 +0100

    Added sampling capabilities for VAE

--- bestiary/mnist.py
+++ bestiary/mnist.py
@@ -1,6 +1,7 @@
 """Demonstrate the use of different autoencoder models for MNIST."""
 
 import argparse
+import torch
 
 from .callbacks import SaveLatentSpaceCallback
 from .data import MNISTDataModule
@@ -51,8 +52,29 @@ if __name__ == '__main__':
             lr=1e-3  # should be made configurable in your own code :-)
         )
 
+    # We will use this to save our sample later on (when using a VAE),
+    # and of course to save all reconstructions during training.
+    save_cb = SaveLatentSpaceCallback(output_dir=f'./output/{args.model}')
+
     trainer = Trainer(
         max_epochs=args.max_epochs,
         callbacks=SaveLatentSpaceCallback(output_dir=f'./output/{args.model}')
     )
     trainer.fit(model, dm)
+
+    # We can only sample images if we are training a VAE. This is
+    # achieved by first sampling a parameterisation of the latent
+    # space, then decode it!
+    if args.model == 'vae':
+        with torch.no_grad():
+            x, y = next(iter(dm.val_dataloader()))
+            batch_size = x.size(0)
+            mu, logvar = model.encode(x.view(batch_size, -1))
+
+            p = torch.distributions.Normal(
+                torch.zeros_like(mu), torch.ones_like(logvar)
+            )
+            z = p.rsample()
+            x_hat = model.decode(z)
+
+            save_cb.save(x_hat, f'./output/{args.model}/Sample.png')
