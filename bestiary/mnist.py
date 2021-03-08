1fec784fe65210733fcdf1aa25872a9c13a6f533
Bastian Rieck <bastian.rieck@bsse.ethz.ch>
2021-03-08 08:45:34 +0100

    Added more parameters to training script

--- bestiary/mnist.py
+++ bestiary/mnist.py
@@ -1,21 +1,58 @@
 """Demonstrate the use of different autoencoder models for MNIST."""
 
+import argparse
+
 from .callbacks import SaveLatentSpaceCallback
 from .data import MNISTDataModule
+
 from .models import LinearAutoencoder
+from .models import BetaVAE
 
 from pytorch_lightning import Trainer
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-m', '--model',
+        type=str,
+        choices=['linear', 'vae'],
+        help='Model to use for training',
+        default='linear'
+    )
+
+    parser.add_argument(
+        '--max-epochs',
+        type=int,
+        # This default value is rather low; it is sufficient to obtain
+        # some 'okayish' results for MNIST, but you want adjust this.
+        default=50,
+        help='Maximum number of epochs to train'
+    )
+
+    args = parser.parse_args()
+
     dm = MNISTDataModule()
     dm.prepare_data()
     dm.setup()
 
-    model = LinearAutoencoder(input_dim=dm.input_dim)
+    if args.model == 'linear':
+        model = LinearAutoencoder(
+            input_dim=dm.input_dim,
+            bottleneck_dim=2,
+            lr=1e-2  # should be made configurable in your own code :-)
+
+        )
+    elif args.model == 'vae':
+        model = BetaVAE(
+            input_dim=dm.input_dim,
+            bottleneck_dim=2,
+            lr=1e-3  # should be made configurable in your own code :-)
+        )
 
     trainer = Trainer(
-        max_epochs=50,
-        callbacks=SaveLatentSpaceCallback(output_dir='./output')
+        max_epochs=args.max_epochs,
+        callbacks=SaveLatentSpaceCallback(output_dir=f'./output/{args.model}')
     )
     trainer.fit(model, dm)
